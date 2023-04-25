7c6f27ae4e93338217468b9e8c8dbe48fd5376f8
NicolasAHF <nicolashernandezf22@gmail.com>
2023-04-25 19:52:18 -0300

    CreateArticle red test Logic

---
+++ Blog.BusinessLogic/ArticleLogic.cs
@@ -0,0 +1,44 @@
+﻿using Blog.Domain.Entities;
+using Blog.IBusinessLogic;
+using Blog.IDataAccess;
+
+namespace Blog.BusinessLogic;
+
+public class ArticleLogic: IArticleLogic
+{
+    private readonly IRepository<Article> _repository;
+    public ArticleLogic(IRepository<Article> articleRepository)
+    {
+        _repository = articleRepository;
+    }
+
+    public Article GetArticleById(Guid articleId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Article> GetAllArticles()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Article CreateArticle(Article article)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Article> GetArticleByText(string text)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Article UpdateArticle(Guid id, Article article, Guid authorization)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteArticle(Guid articleId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Blog.Tests/BusinessLogicTests/ArticleLogicTests.cs
@@ -0,0 +1,82 @@
+﻿using Blog.BusinessLogic;
+using Blog.Domain.Entities;
+using Blog.Domain.Enums;
+using Blog.IBusinessLogic;
+using Blog.IDataAccess;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace Blog.Tests.BusinessLogicTests;
+
+[TestClass]
+public class ArticleLogicTests
+{
+    private Mock<IRepository<Article>> _articleRepoMock;
+    private Article _articleTest;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _articleRepoMock = new Mock<IRepository<Article>>(MockBehavior.Strict);
+        
+        var user = new User()
+        {
+            Id = Guid.NewGuid(),
+            FirstName = "Nicolas",
+            LastName = "Hernandez",
+            Username = "NicolasAHF",
+            Password = "123456",
+            Roles = new List<UserRole>{},
+            Email = "nicolas@example.com"
+        };
+
+        var role = new UserRole()
+        {
+            Role = Role.Blogger,
+            UserId = user.Id,
+            User = user
+        };
+        
+        user.Roles.Add(role);
+        
+        var formFile = new Mock<IFormFile>();
+        formFile.Setup(f => f.Length).Returns(1234);
+        formFile.Setup(f => f.FileName).Returns("test.jpg");
+        formFile.Setup(f => f.ContentType).Returns("image/jpeg");
+        
+        using var ms = new MemoryStream();
+        var image = ms.ToArray();
+        
+        var _articleTest = new Article()
+        {
+            Id = Guid.NewGuid(),
+            Title = ".NET 6 Webpage",
+            Content = "New features about .NET are being developed",
+            Owner = user,
+            Comments = new List<Comment>(){},
+            DateLastModified = DateTime.Now,
+            DatePublished = DateTime.Now,
+            Image = image,
+            IsPublic = true,
+            Template = Template.RectangleTop
+            
+        };
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _articleRepoMock.VerifyAll();
+    }
+
+    [TestMethod]
+    public void CreateArticleSuccessTest()
+    {
+        var logic = new ArticleLogic(_articleRepoMock.Object);
+        _articleRepoMock.Setup(o => o.Insert(It.IsAny<Article>()));
+        _articleRepoMock.Setup(o => o.Save());
+        var result = logic.CreateArticle(_articleTest);
+        Assert.AreEqual(_articleTest, result);
+    }
+    
+}
