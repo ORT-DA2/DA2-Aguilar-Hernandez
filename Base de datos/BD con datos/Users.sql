688d4c6077b2a2157de39436504d6043deefeba7
NicolasAHF <nicolashernandezf22@gmail.com>
2023-05-03 14:09:56 -0300

    Base de datos con datos

---
+++ Base de datos/BD con datos/Users.sql
@@ -0,0 +1,7 @@
+insert into dbo.Users (Id, FirstName, LastName, Username, Password, Email)
+values  (N'57585b1d-e35e-4e3a-dde7-08db4b6097f5', N'Nicolas', N'Hernandez', N'NicolasAHF', N'123456', N'nicolas@example.com'),
+        (N'6d1ac1e6-6c0e-47fe-0b2d-08db4b615657', N'Jose Antonio', N'Hernandez', N'Jose1234', N'123456', N'firstName@example.com'),
+        (N'7f1b3d10-23db-4c58-99ea-08db4b87a685', N'Monica', N'Lopez', N'Monica123', N'123456', N'monica@example.com'),
+        (N'de945781-eb69-41fd-15b3-08db4bef102b', N'Ramiro', N'Fernandez', N'Ramiro', N'123456', N'rama123@example.com'),
+        (N'20b0de82-920b-4140-15b4-08db4bef102b', N'Mario', N'Fernandez', N'Mario123', N'123456', N'mario123@example.com'),
+        (N'b75bdc5e-5bbe-4def-7100-08db4bf52f41', N'Matias actualizado', N'Blanco', N'Matias1', N'123456', N'matias1@example.com');
