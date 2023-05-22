5fe2ffd5c7dd67a38eb8dd108350cf63a17fb609
NicolasAHF <nicolashernandezf22@gmail.com>
2023-05-22 16:35:48 -0300

    No notifications message added

--- Codigo Fuente Front-end/Blog-Frontend/src/app/header/header.component.ts
+++ Codigo Fuente Front-end/Blog-Frontend/src/app/header/header.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   isLoggedIn = false;
   notifications: any[] = [];
+  hasNotifications = false;
+
+  ngOnInit() {
+    this.checkNotifications();
+  }
+
+  checkNotifications() {
+    this.hasNotifications = this.notifications.length > 0;
+  }
 
   onSearch(event: Event) {
     event.preventDefault();
